6380c1fdf42d023f7a103260ba204efbe37d5c0f
Shahed Nasser <shahednasser@gmail.com>
2025-12-22 10:51:01 +0200

    fix(medusa): allow passing null to cart API routes (#14381)
    Allow passing `null` as the locale for cart API route, as `optional` only allow passing `undefined`.

--- packages/medusa/src/api/store/carts/validators.ts
+++ packages/medusa/src/api/store/carts/validators.ts
@@ -23,7 +23,7 @@ export const CreateCart = z
     sales_channel_id: z.string().nullish(),
     promo_codes: z.array(z.string()).optional(),
     metadata: z.record(z.unknown()).nullish(),
-    locale: z.string().optional(),
+    locale: z.string().nullish(),
   })
   .strict()
 export const StoreCreateCart = WithAdditionalData(CreateCart)
@@ -58,7 +58,7 @@ export const UpdateCart = z
     sales_channel_id: z.string().nullish(),
     metadata: z.record(z.unknown()).nullish(),
     promo_codes: z.array(z.string()).optional(),
-    locale: z.string().optional(),
+    locale: z.string().nullish(),
   })
   .strict()
 export const StoreUpdateCart = WithAdditionalData(UpdateCart)
